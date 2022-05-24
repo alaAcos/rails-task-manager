776a1942f61501f0e9aa025be93057ca2d369115
Adrien Larbouillat <ala@acoservices.eu>
2022-05-24 15:32:10 +0200

    5.1 done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,4 +3,12 @@ class TasksController < ApplicationController
   def list
     @tasks = Task.all
   end
+
+  def find
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @new_task = Task.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   Rails.application.routes.draw do
-    get 'tasks', to: 'tasks#list'
+    get '/tasks', to: 'tasks#list'
+    get '/tasks/:id', to: 'tasks#find'
+    get '/new', to: 'tasks#new'
   end
 end
