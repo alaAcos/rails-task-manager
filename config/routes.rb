8d0ba8d467cd412e094724410e1f5b99f29d494a
Adrien Larbouillat <ala@acoservices.eu>
2022-05-24 16:49:04 +0200

    6 done

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   Rails.application.routes.draw do
-    get '/tasks', to: 'tasks#list'
-    get '/tasks/:id', to: 'tasks#find'
-    get '/new', to: 'tasks#new'
+    # CREATE
+    get '/tasks/new', to: 'tasks#new'
     post '/tasks', to: 'tasks#create'
+    # READ
+    get '/tasks', to: 'tasks#list'
+    get '/tasks/:id', to: 'tasks#find', as: :task
+    # UPDATE
+    get '/tasks/:id/edit', to: 'tasks#edit'
+    patch '/tasks/:id', to: 'tasks#update'
   end
 end
